d465f8600937928827524a41e34a224c7609fd2d
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-22 21:44:56 -0500

    Sign in and registration complete

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
             <Route path="/" element={<LoginPage />} />
             <Route
               path="/home"
-              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+              element={true ? <HomePage /> : <Navigate to="/" />}
             />
             <Route
               path="/profile/:userId"
